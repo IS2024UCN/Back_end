d6e3a98009ce8216f3e98d8c854c987f9fcd2d1b
VichoFL <vicente.flores@alumnos.ucn.cl>
2024-11-12 22:37:49 -0300

    fix(): Se agregan lineas de codigo

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
-Route::get('userlogged', [\App\Http\Controllers\AuthController::class, 'userLogged'])->middleware(['api', 'auth']);
-Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout'])->middleware(['api', 'auth']);
+Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
+Route::get('/userlogged', [\App\Http\Controllers\AuthController::class, 'userLogged'])->middleware(['api', 'auth']);
+Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout'])->middleware(['api', 'auth']);
 
 // Ruta para el registro de usuarios
 Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register'])->name('register');
