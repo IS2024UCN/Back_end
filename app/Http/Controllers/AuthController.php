9b73156e49f3639dac5a4647b4010be56b922efc
LuisSolizHuayta <luis.soliz@alumnos.ucn.cl>
2024-12-08 19:17:11 -0300

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,13 +8,15 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Illuminate\Support\Facades\Hash;
-use App\Models\Product;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Log;
+
 
 class AuthController extends BaseController
 {
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $this->middleware('auth:api', ['except' => ['login','register']]);
     }
 
     public function login (Request $request)
@@ -33,6 +35,24 @@ class AuthController extends BaseController
             'email.email' => 'Formato incorrecto de correo',
             'password.required' => 'Contraseña es obligatoria'
         ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user){
+            return response([
+                'message' => 'Las credenciales de acceso son incorrectas o el usuario no está registrado en el sistema',
+                'error' => true
+            ], 401);
+        }
+
+        if(!$user->is_active){
+            return response([
+                'message' => 'Usuario deshabilitado. Contacte al administrador',
+                'error' => true 
+            ], 403);
+        }
+
+
         // Intento de auteticacion
         $token = Auth::attempt($request->only('email','password'));
 
@@ -51,8 +71,7 @@ class AuthController extends BaseController
             'message' => 'Inicio de sesion exitoso',
             'data' => [
                 'token' => $token,
-                'user' => $user,
-                'user_role' => $user->role_id
+                'user' => $user
             ],
             'error' => false
         ]);}
@@ -66,11 +85,52 @@ class AuthController extends BaseController
         }
     }
 
+    public function updatePassword(Request $request){
+        try{
+            $request->validate([
+                'current_password' => 'required',
+                'new_password' => [
+                    'required',
+                    'min:8',
+                    'confirmed',
+                    'regex:/[A-Z]/',
+                    'regex:/[a-z]/',
+                    'regex:/[0-9]/',
+                    'regex:/[@$!%*#?&]/'
+                ],
+            ]);
+
+            $user = Auth::user();
+
+            if(!Hash::check($request->current_password, $user->password)){
+                return response()->json([
+                    'error' => 'Contraseña actual incorrecta'],
+                    400);
+            }
+            if($request->current_password === $request->new_password){
+                return response()->json([
+                    'error' => 'La nueva contraseña no puede ser igual a la actual'],
+                    400);
+            }
+
+            User::where('id', $user->id)->update(['password' => Hash::make($request->new_password)]);
+
+            return response()->json([
+                'message' => 'Contraseña actualizada correctamente'],
+            200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => 'Error al actualizar la contraseña',
+                'details' => $e->getMessage()],
+                500);
+        }
+    }
+
+
     public function logout (){
         try{
             Auth::logout();
             return response([
-                // Mensaje de cierre de sesion
                 'message' => 'Sesion Cerrada',
                 'data' => [],
                 'error' => false
@@ -78,7 +138,6 @@ class AuthController extends BaseController
     }
         catch (\Exception $e) {
             return response([
-            // Mensaje de error al cerrar sesion
               'message' => 'Error al cerrar sesion',
                'data' => [],
               'error' => $e->getMessage()
@@ -88,7 +147,6 @@ class AuthController extends BaseController
 
     public function UserLogged(){
         return response([
-            // Mensaje de usuario autenticado
             'message' => 'Usuario Autenticado',
             'data' => [
                 'user' => Auth::user()
@@ -97,7 +155,128 @@ class AuthController extends BaseController
         ], 200);
     }
 
-    
-    
+    public function register(Request $request)
+    {
+        try {
+            // Validar los datos de entrada
+            $request->validate([
+                'rut' => ['required', 'string', 'unique:users', 'regex:/^[0-9]+[Kk0-9]$/', function($attribute, $value, $fail){
+                }],
+                'name' => ['required', 'string', 'min:3', 'regex:/^[a-zA-Z\s]+$/', function($attribute, $value, $fail){
+                    if (preg_match('/[0-9]/', $value)) {
+                        $fail('El nombre no puede contener números');
+                    }
+                }],
+                'last_name' => ['required', 'string', 'min:3', 'regex:/^[a-zA-Z\s]+$/', function($attribute, $value, $fail){
+                    if (preg_match('/[0-9]/', $value)) {
+                        $fail('El apellido no puede contener números');
+                    }
+                }],
+                'phone' => ['required', 'string', 'regex:/^[0-9]{9}$/', function($attribute, $value, $fail){
+                }],
+                
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users', function($attribute, $value, $fail){
+                    if(!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                        $fail('Formato incorrecto de correo');
+                    }
+                }],
+            ], [
+                'rut.unique' => 'Este RUT ya esta registrado en el sistema. Intente iniciar sesión.',
+                'rut.regex' => 'El RUT ingresado no es válido.',
+                'rut.required' => 'RUT requerido',
+                'email.unique' => 'Este correo electrónico ya esta registrado en el sistema. Intente iniciar sesión.',
+                'email.email' => 'Este correo electrónico no es válido.',
+                'email.required' => 'Correo requerido',
+                'phone.regex' => 'El teléfono móvil ingresado no es válido.',
+                'phone.required' => 'Telefono requrido',
+                'name.min' => 'Los nombres o apellidos deben tener más de 2 caracteres.',
+                'name.required' => 'Nombre requerido',
+                'name.regex' => 'El nombre no puede contener números.',
+                'last_name.min' => 'Los nombres o apellidos deben tener más de 2 caracteres.',
+                'last_name.required' => 'Apellido requerido',
+                'last_name.regex' => 'El apellido no puede contener números.'
+                
+                
+            ]); 
 
+            // Validar el RUT chileno
+            $rut = strtoupper($request->input('rut'));
+            if (!$this->validateRut($rut)) {
+                return response([
+                    'message' => 'El RUT no es válido',
+                    'data' => [],
+                    'error' => true
+                ], 422);
+            }
+            // Convertir el RUT a mayúsculas
+            $rut = strtoupper($rut);
+
+            // Agregar el prefijo +56 al teléfono
+            $phone = '+56' . $request->input('phone');
+
+            $name = strtolower($request->input('name'));
+            $last_name = strtolower($request->input('last_name'));
+            
+            // Crear el usuario
+            $user = User::create([
+                'rut' => $rut,
+                'name' => $name . ' ' . $last_name,
+                'phone' => $phone,
+                'email' => $request->input('email'),
+                'password' => bcrypt($rut)
+            ]);
+            
+            // Generar un token de acceso para el usuario
+            $token = JWTAuth::fromUser($user);         
+
+            return response([
+                'message' => 'Usuario registrado exitosamente',
+                'data' => [
+                    'user' => $user,
+                    'token' => $token
+                ],
+                'error' => false
+            ], 201);
+        } catch (\Exception $e) {
+            return response([
+                'message' => 'Error al registrar el usuario',
+                'data' => [],
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    private function validateRut($rut)
+    {
+        // Eliminar puntos y guiones
+        $rut = str_replace(['.', '-'], '', strtoupper($rut));
+        $number = substr($rut, 0, -1);
+        $dv = substr($rut, -1);
+
+        // Validar que el RUT tenga el formato correcto
+        if (!preg_match('/^[0-9]+[K0-9]$/', $rut)) {
+            return false;
+        }
+
+        // Calcular el dígito verificador
+        $sum = 0;
+        $factor = 2;
+        for ($i = strlen($number) - 1; $i >= 0; $i--) {
+            $sum += $number[$i] * $factor;
+            $factor = $factor == 7 ? 2 : $factor + 1;
+        }
+        $dv_calculated = 11 - ($sum % 11);
+        if ($dv_calculated == 11) {
+            $dv_calculated = '0';
+        } elseif ($dv_calculated == 10) {
+            $dv_calculated = 'K';
+        } else {
+            $dv_calculated = (string) $dv_calculated;
+        }
+
+        // Comparar el dígito verificador calculado con el proporcionado
+        return $dv_calculated === $dv;
+    }
 }
+
+
