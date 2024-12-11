c0c0bf801c0672f4a82c88c12c0a60bc661c0f37
LuisSolizHuayta <luis.soliz@alumnos.ucn.cl>
2024-12-10 21:52:00 -0300

    Feat(): agregar soporte para ISBN diferenciado entre libros y peliculas
    - Se modifico el metodo registerProduct para manejar dos campos ISBN:
        - ISBN_books para libros
        - ISBN_movies para peliculas
    - Se ajusto la logica para asignar NULL al ISBN no utilizado, dependiendo del tipo de producto
    - Se realizaron validaciones adicionales para asegurar la unicidad y consistencia de los ISBN

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -51,7 +51,8 @@ class ProductController extends Controller
             $validatedData = $request->validate([
                 'title' => 'required|string|max:255',
                 'creator' => 'required|string|max:255',
-                'ISBN' => 'required|string|unique:products,ISBN',
+                'ISBN_books' => 'required_if:type,libro|nullable|string|unique:products,ISBN_books',
+                'ISBN_movies' => 'required_if:type,pelicula|nullable|string|unique:products,ISBN_movies',
                 'publisher' => 'nullable|string|max:255',
                 'release_date' => 'nullable|date',
                 'rental_price' => 'required|numeric|min:0',
@@ -61,7 +62,6 @@ class ProductController extends Controller
             $product = new Product();
             $product->title = $validatedData['title'];
             $product->creator = $validatedData['creator'];
-            $product->ISBN = $validatedData['ISBN'];
             $product->publisher = $validatedData['publisher'] ?? null;
             $product->release_date = $validatedData['release_date'] ?? null;
             $product->rental_price = $validatedData['rental_price'];
@@ -69,6 +69,14 @@ class ProductController extends Controller
             $product->available_stock = $validatedData['initial_stock'];
             $product->type = $validatedData['type'];
             $product->is_enabled = true;
+
+            if ($validatedData['type'] === 'libro') {
+                $product->ISBN_books = $validatedData['ISBN_books'];
+                $product->ISBN_movies = null;
+            } elseif ($validatedData['type'] === 'pelicula') {
+                $product->ISBN_movies = $validatedData['ISBN_movies'];
+                $product->ISBN_books = null;
+            }
 
             $product->save();
 
